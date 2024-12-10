3a2a0d0e8ef97175aafec57d738c242662d75441
fernando.jaramillo <fernando.jaramillo@pragma.com.co>
2024-12-10 12:32:54 -0500

    fix(app) Se ajusta configuiracione y deprecated en angular 17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { ApplicationConfig } from '@angular/platform-browser';
 
+/**
+ * Configuración de la aplicación, Opciones de configuración para bootstrapApplication en main.ts. providers: Proveedores de servicios para la aplicación.
+ * @export
+ * @class AppConfig
+ * @implements {ApplicationConfig}
+ * @returns {ApplicationConfig} AppConfig
+ */
 export const AppConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi()), provideAnimations()],
+  providers: [provideRouter(routes, withComponentInputBinding()), provideHttpClient(withInterceptorsFromDi()), provideAnimations()],
 };
